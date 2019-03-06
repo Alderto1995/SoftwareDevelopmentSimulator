3242d350485895c729d0787b06d104c341e8a3cb
poli 1600 <poli1600@nhl.nl>
2019-03-06 15:29:39 +0100

    Receiver en Publisher toegevoegd

---
+++ Simulator/Kruispunt Simulator/Assets/Scripts/Publisher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+public class Publisher : MonoBehaviour
+{
+    MqttClient client;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string broker = "broker.0f.nl";
+        string topic = "1";
+        string clientId = "test";
+        string message = "Hello from C#!";
+
+        try
+        {
+            client = new MqttClient(broker);
+            client.Connect(clientId);
+            client.Publish(topic, Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, false);
+            Debug.Log($"Message sent: {message}");
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Simulator/Kruispunt Simulator/Assets/Scripts/Receiver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+public class Receiver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MqttClient client;
+        string broker = "broker.0f.nl";
+        string topic = "1";
+        string clientId = "test";
+
+        try
+        {
+            client = new MqttClient(broker);
+            client.Connect(clientId);
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            Debug.Log("Jooo Verbonden");
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+        }
+    }
+
+
+    static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        Debug.Log(Encoding.UTF8.GetString(e.Message));
+        // handle message received 
+    }
+
+    void Update()
+    {
+
+    }
+}
