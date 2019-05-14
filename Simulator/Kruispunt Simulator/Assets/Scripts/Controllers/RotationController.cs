9864025380000e82a88645c027fcfc0cacbe66d7
poli 1600 <poli1600@nhl.nl>
2019-05-14 19:30:35 +0200

    Unity Update & update 2.1
    Alles behalve de sensoren bij het kruispunt is nu volgens het diagram.
    Slagbomen toegevoegd.
    Grote sensoren toegevoegd.

---
+++ Simulator/Kruispunt Simulator/Assets/Scripts/Controllers/RotationController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+public class RotationController : Receiver
+{
+    public enum RotationAxis { X, Y, Z }
+
+    public int groupId;
+    public int componentId;
+    public string userType;
+    public string componentType;
+    public float degreesPerSecond = 9f;
+    public int maxRotation = 90;
+    public int minRotation = 0;
+    public RotationAxis axis;
+
+    public bool open;
+
+    void Awake()
+    {
+        topic = $"{userType}/{groupId}/{componentType}/{componentId}";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+            Rotate(maxRotation);
+        }
+        else
+        {
+            Rotate(minRotation);
+        }
+    }
+
+    private void Rotate(float angle)
+    {
+        if (axis == RotationAxis.X)
+        {
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(angle, transform.localRotation.y, transform.localRotation.z), degreesPerSecond * Time.deltaTime);
+        }
+        else if (axis == RotationAxis.Y)
+        {
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(transform.localRotation.x, angle, transform.localRotation.z), degreesPerSecond * Time.deltaTime);
+        }
+        if (axis == RotationAxis.Z)
+        {
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, angle), degreesPerSecond * Time.deltaTime);
+        }
+    }
+
+    protected override void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        string message = Encoding.UTF8.GetString(e.Message);
+        Debug.Log($"Received: {message}");
+        int.TryParse(message, out int value);
+
+        switch (value)
+        {
+            case 0:
+                open = false;
+                break;
+            case 1:
+                open = true;
+                break;
+        }
+    }
+}
