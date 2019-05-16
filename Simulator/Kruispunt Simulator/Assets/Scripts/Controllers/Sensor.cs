ac0548761814c4652ef4ae91229f833f27953e2a
poli 1600 <poli1600@nhl.nl>
2019-05-16 15:36:03 +0200

    Laatste aanpassingen

--- Simulator/Kruispunt Simulator/Assets/Scripts/Controllers/Sensor.cs
+++ Simulator/Kruispunt Simulator/Assets/Scripts/Controllers/Sensor.cs
@@ -8,33 +8,63 @@ public class Sensor : MonoBehaviour
     public int groupId;
     public int componentId;
     public string userType;
+    public Transform subSensorTransform;
 
     private Collider collider;
-    private int carCounter;
+    private SubSensor subSensor;
+    private int counter;
 
     // Start is called before the first frame update
     void Start()
     {
         collider = GetComponent<Collider>();
         collider.isTrigger = true;
-        carCounter = 0;
+
+        if(subSensorTransform != null)
+        {
+            subSensor = subSensorTransform.GetComponent<SubSensor>();
+        }
+        else
+        {
+            subSensor = null;
+        }
+
+        counter = 0;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(carCounter <= 0)
+        if(counter <= 0)
         {
             Publisher.instance.SendMessage($"{userType}/{groupId}/sensor/{componentId}", "1");
         }
-        carCounter++;
+        counter++;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(carCounter == 1)
+        if(counter == 1)
         {
             Publisher.instance.SendMessage($"{userType}/{groupId}/sensor/{componentId}", "0");
         }
-        carCounter--;
+        counter--;
+    }
+
+    public void Entered()
+    {
+        if(counter <= 0)
+        {
+            Publisher.instance.SendMessage($"{userType}/{groupId}/sensor/{componentId}", "1");
+        }
+        counter++;
+    }
+
+    public void Left()
+    {
+        if (counter == 1)
+        {
+            Publisher.instance.SendMessage($"{userType}/{groupId}/sensor/{componentId}", "0");
+        }
+        counter--;
     }
 }
---
+++ Simulator/Kruispunt Simulator/Assets/Scripts/Controllers/SubSensor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubSensor : MonoBehaviour
+{
+    public Transform parentSensorTransform;
+
+    private Collider collider;
+    private Sensor parentSensor;
+
+    void Start()
+    {
+        collider = GetComponent<Collider>();
+        collider.isTrigger = true;
+
+        parentSensor = parentSensorTransform.GetComponent<Sensor>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        parentSensor.Entered();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        parentSensor.Left();
+    }
+}
